9785ac0a896335a261751b9401ecf6c12807bfb2
AmaliJD <60673543+AmaliJD@users.noreply.github.com>
2020-12-06 19:11:50 -0500

    ChooseOne Node

---
+++ Assets/Core/Scripts/Behavior/TreeSharpPlus/ChooseOne.cs
@@ -0,0 +1,73 @@
+﻿#region License
+
+// A simplistic Behavior Tree implementation in C#
+// Copyright (C) 2010-2011 ApocDev apocdev@gmail.com
+// 
+// This file is part of TreeSharp
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#endregion
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TreeSharpPlus
+{
+    /// <summary>
+    ///   The base sequence class. This will execute each branch of logic, in order, at a probability of 50%.
+    ///   If all branches succeed, this composite will return a successful run status.
+    ///   If any branch fails, this composite will return a failed run status.
+    /// </summary>
+    public class ChooseOne : NodeGroup
+    {
+        public ChooseOne(params Node[] children)
+            : base(children)
+        {
+        }
+
+        public override IEnumerable<RunStatus> Execute()
+        {
+            int select = Random.Range(0, this.Children.Count);
+            Node node = this.Children[select];
+
+            // Move to the next node
+            this.Selection = node;
+            node.Start();
+
+            // If the current node is still running, report that. Don't 'break' the enumerator
+            RunStatus result;
+            while ((result = this.TickNode(node)) == RunStatus.Running)
+                yield return RunStatus.Running;
+
+            // Call Stop to allow the node to clean anything up.
+            node.Stop();
+
+            // Clear the selection
+            this.Selection.ClearLastStatus();
+            this.Selection = null;
+
+            if (result == RunStatus.Failure)
+            {
+                yield return RunStatus.Failure;
+                yield break;
+            }
+
+            yield return RunStatus.Running;
+
+            yield return RunStatus.Success;
+            yield break;
+        }
+    }
+}
--- Assets/InteractiveBehaviorTree.cs
+++ Assets/InteractiveBehaviorTree.cs
@@ -8,118 +8,132 @@ public class InteractiveBehaviorTree : MonoBehaviour
 	public Transform meetingPoint;
 	public GameObject[] people;
 	private string[] moods;
-
-	//private
+	private float timer = 0;
 
 	private BehaviorAgent behaviorAgent;
 
 	// Use this for initialization
 	void Start()
 	{
-		behaviorAgent = new BehaviorAgent(this.BuildTreeRoot());
+		moods = new string[people.Length];
+
+		behaviorAgent = new BehaviorAgent(BuildTreeRoot());
 		BehaviorManager.Instance.Register(behaviorAgent);
 		behaviorAgent.StartBehavior();
-
-		moods = new string[people.Length];
+	}
+	void Update()
+	{
+		timer += Time.deltaTime;
 	}
 
-	protected Node ST_ApproachAndWait(Transform target)
+	#region Affordances
+	protected Node GoTo(GameObject person, Transform target)
 	{
+		Debug.Log("\t      GoTo");
 		Val<Vector3> position = Val.V(() => target.position);
-		return new Sequence(
-			new LeafInvoke(() => {
-				foreach(GameObject person in people)
-				{
-					person.GetComponent<BehaviorMecanim>().Node_GoTo(position);
-				}
-			}),
-			new LeafWait(100)
+
+		return new Sequence
+		(
+			person.GetComponent<BehaviorMecanim>().Node_GoToUpToRadius(position, 2f)
 		);
 	}
-
-	protected Node BuildTreeRoot()
+	protected Node Wave(GameObject person)
 	{
-		Node roaming = new Sequence(
-						new Sequence(
-								Input(),
-								MaintainArcs(),
-								Story()
-							)
-						);
-		return roaming;
-	}
+		Debug.Log("\t      Wave");
 
-	protected Node Input()
-	{
-		return new DecoratorLoop(
-			new LeafInvoke(() => {
-				for (int i = 0; i <= 3; i++)
-				{
-					/*if (UserMoveScript.boxes.Contains(bounds[i]))
-					{
-						Blackboard.UserInput.sceneNumber = i;
-
-						return;
-					}*/
-					Blackboard.UserInput.sceneNumber = 0;
-				}
-			})
+		return new Sequence
+		(
+			person.GetComponent<BehaviorMecanim>().ST_PlayHandGesture(Val.V(() => "WAVE"), Val.V(() => (long)10))
 		);
 	}
+	protected Node Dance(GameObject person)
+	{
+		Debug.Log("\t      Dance");
+
+		return new Sequence
+		(
+			person.GetComponent<BehaviorMecanim>().Node_BodyAnimation(Val.V(() => "Breakdance"), Val.V(() => true))
+		);
+	}
+	protected Node Suprised(GameObject person, int index)
+	{
+		Debug.Log("\t      Suprised");
+		moods[index] = "suprised";
+
+		return new Sequence
+		(
+			person.GetComponent<BehaviorMecanim>().Node_BodyAnimation(Val.V(() => "Suprised"), Val.V(() => true))
+		);
+	}
+	protected Node Bored(GameObject person, int index)
+	{
+		Debug.Log("\t      Bored");
+		moods[index] = "bored";
+
+		return new Sequence
+		(
+			person.GetComponent<BehaviorMecanim>().Node_BodyAnimation(Val.V(() => "Yawn"), Val.V(() => true))
+		);
+	}
+	#endregion
 
 	protected Node CheckArc(int i)
 	{
+		Debug.Log("CHECK ARC");
 		return new Sequence(
-			new LeafAssert(() => i == Blackboard.UserInput.sceneNumber),
+			new LeafAssert(() => i == Blackboard.Input.sceneNumber),
 			new LeafInvoke(() => {
 				Blackboard.StoryArcs.currArc = (Blackboard.StoryArc)i;
 			})
 		);
 	}
-	protected Node MaintainArcs()
-	{
-		return new DecoratorLoop(
-			//new Selector(
-			//    CheckArc(-1),
-			//    CheckArc(0),
-			//    CheckArc(1),
-			//    CheckArc(2)
-			//)
-			new LeafInvoke(() => {
-				switch (Blackboard.UserInput.sceneNumber)
-				{
-					case -1:
-						Blackboard.StoryArcs.currArc = Blackboard.StoryArc.STORY_0;
-						break;
-					case 0:
-						Blackboard.StoryArcs.currArc = Blackboard.StoryArc.STORY_1;
-						break;
-					case 1:
-						Blackboard.StoryArcs.currArc = Blackboard.StoryArc.STORY_2;
-						break;
-					case 2:
-						Blackboard.StoryArcs.currArc = Blackboard.StoryArc.STORY_3;
-						break;
-				}
-			})
-		);
-	}
+	
 
 	protected Node Behavior(int i)
 	{
-		//if(i == 0)
-        //{
-			Node action = new DecoratorLoop(
-						new SequenceParallel(
-							ST_ApproachAndWait(meetingPoint)
-						)) ;
+		Node action = new Sequence();
 
-			return action;
-		//}
+		if (i == 0)
+		{
+			Debug.Log("\t Executing MEETUP");
+			action = new Sequence(
+						new SequenceParallel(
+							GoTo(people[0], meetingPoint),
+							GoTo(people[1], meetingPoint),
+							GoTo(people[2], meetingPoint)
+						),
+
+						new SequenceParallel(
+							Wave(people[0]),
+							Wave(people[1]),
+							Wave(people[2])
+						),
+
+						new LeafWait(1000)
+					);
+		}
+		else if (i == 1)
+		{
+			Debug.Log("\t Executing DANCE");
+			action = new Sequence(
+						new ChooseOne(
+							Dance(people[0]), Suprised(people[0], 0), Bored(people[0], 0)
+						),
+						new ChooseOne(
+							Dance(people[1]), Suprised(people[1], 1), Bored(people[1], 1)
+						),
+						new ChooseOne(
+							Dance(people[2]), Suprised(people[2], 2), Bored(people[2], 2)
+						)
+					);
+		}
+
+		return action;
 	}
 
 	protected Node SelectStory(int i)
 	{
+		Debug.Log("\t Selecting Story " + i);
 		return new SelectorParallel(
 			new DecoratorInvert(new DecoratorLoop(new Sequence(
 				new LeafAssert(() => Blackboard.StoryArcs.currArc == (Blackboard.StoryArc)Enum.Parse(typeof(Blackboard.StoryArc), "STORY_" + i))
@@ -131,21 +145,108 @@ public class InteractiveBehaviorTree : MonoBehaviour
 		);
 	}
 
-	protected Node Story()
+	#region Story Orginization
+	protected Node BuildTreeRoot()
 	{
+		Node roaming = new DecoratorLoop(
+						new Sequence(
+								//new LeafInvoke(() => { Debug.Log("LOOPING AGAIN"); }),
+								//new DecoratorForceStatus(RunStatus.Success, Input()),
+								//new DecoratorForceStatus(RunStatus.Success, MaintainArcs()),
+								//new DecoratorForceStatus(RunStatus.Success, Story())
+								Behavior(0), Behavior(1)
+							)
+						);
+		return roaming;
+	}
+	protected Node Input()
+	{
+		Debug.Log("INPUT PHASE");
 		return new Sequence(
-					SelectStory(0),
-					SelectStory(1),
-					SelectStory(2),
-					SelectStory(3)
+			new LeafInvoke(() => {
+				if(timer > 90)
+                {
+					Blackboard.Input.sceneNumber = 3;
+					//Debug.Log("\t Scene Number " + 3);
+				}
+				else if (timer > 60)
+                {
+					Blackboard.Input.sceneNumber = 2;
+					//Debug.Log("\t Scene Number " + 2);
+				}
+				else if (timer > 20)
+				{
+					Blackboard.Input.sceneNumber = 1;
+					//Debug.Log("\t Scene Number " + 1);
+				}
+				else if (timer > 0)
+				{
+					Blackboard.Input.sceneNumber = 0;
+					//Debug.Log("\t Scene Number " + 0);
+				}
+			})
 		);
 	}
-
-	private struct Blackboard
+	protected Node MaintainArcs()
 	{
-		public struct UserInput
+		Debug.Log("MAINTAIN ARCS PHASE");
+		return new Sequence(
+			new Selector(
+				CheckArc(0),
+				CheckArc(1),
+				CheckArc(2),
+				CheckArc(3)
+			));
+			/*new LeafInvoke(() => {
+				switch (Blackboard.Input.sceneNumber)
+				{
+					case 0:
+						Blackboard.StoryArcs.currArc = Blackboard.StoryArc.STORY_0;
+						break;
+					case 1:
+						Blackboard.StoryArcs.currArc = Blackboard.StoryArc.STORY_1;
+						break;
+					case 2:
+						Blackboard.StoryArcs.currArc = Blackboard.StoryArc.STORY_2;
+						break;
+					case 3:
+						Blackboard.StoryArcs.currArc = Blackboard.StoryArc.STORY_3;
+						break;
+				}
+			})
+		);*/
+	}
+
+	protected Node Story()
+	{
+		Debug.Log("STORY PHASE");
+		return new Sequence(new Sequence(
+			new LeafInvoke(() => {
+				switch (Blackboard.StoryArcs.currArc)
+				{
+					case Blackboard.StoryArc.STORY_0:
+						SelectStory(0);
+						break;
+					case Blackboard.StoryArc.STORY_1:
+						SelectStory(1);
+						break;
+					case Blackboard.StoryArc.STORY_2:
+						SelectStory(2);
+						break;
+					case Blackboard.StoryArc.STORY_3:
+						SelectStory(3);
+						break;
+				}
+			})
+		));
+	}
+    #endregion
+
+    private struct Blackboard
+	{
+		public struct Input
 		{
-			public static int sceneNumber = -1;
+			public static int sceneNumber = 0;
 		}
 
 		public struct StoryArcs
